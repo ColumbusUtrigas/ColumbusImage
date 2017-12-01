e7d407fed053eb4ed29f8587754bc5ee2449c0f7
ColumbusTech <ColumbusTech@yandex.ru>
2017-12-01 17:16:50 +0300

    Upgrated BMP loader

--- ClmbsImg_BMP.c
+++ ClmbsImg_BMP.c
@@ -1,11 +1,127 @@
 #include "ColumbusImage.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
+
+	int ReadBytes(void* data, size_t size, FILE* fp)
+	{
+		if (fread(data, size, 1, fp) != 1) return 0;
+		return 1;
+	}
+
+	int ReadUint8(uint8_t* data, FILE* fp)
+	{
+		return ReadBytes(data, sizeof(uint8_t), fp);
+	}
+
+	int ReadUint16(uint16_t* data, FILE* fp)
+	{
+		return ReadBytes(data, sizeof(uint16_t), fp);
+	}
+
+	int ReadUint32(uint32_t* data, FILE* fp)
+	{
+		return ReadBytes(data, sizeof(uint32_t), fp);
+	}
+
+	int ReadInt32(int32_t* data, FILE* fp)
+	{
+		return ReadBytes(data, sizeof(int32_t), fp);
+	}
+
+	typedef struct
+	{
+		uint8_t magic[2]; //Magic Bytes 'B' and 'M'
+		uint32_t size; //Size of whole file
+		uint32_t unused; //Should be 0
+		uint32_t offset; //Offset to bitmap data
+	} BMP_HEADER;
+
+	typedef struct
+	{
+		uint32_t infosize; //Size of info struct
+		int32_t width; //Width of image
+		int32_t height; //Height of image
+		uint16_t planes; //Should be 1
+		uint16_t bits; //Bits per pixel (1, 4, 8, 16, 24, 32)
+		uint32_t compression; //0 = none, 1 = 8-bit RLE, 2 = 4-bit RLE
+	} BMP_INFO;
+
+	typedef struct
+	{
+		uint8_t b; //Blue channel
+		uint8_t g; //Green channel
+		uint8_t r; //Red channel
+		uint8_t a; //Alpha channel
+	} BMP_PALETTE;
+
+	bool ReadHeader(BMP_HEADER* header, FILE* fp)
+	{
+		if (header == NULL || fp == NULL) return false;
+
+		if (!ReadUint8(&header->magic[0], fp)) return false;
+		if (!ReadUint8(&header->magic[1], fp)) return false;
+		if (!ReadUint32(&header->size, fp)) return false;
+		if (!ReadUint32(&header->unused, fp)) return false;
+		if (!ReadUint32(&header->offset, fp)) return false;
+
+		return true;
+	}
+
+	bool ReadInfo(BMP_INFO* info, FILE* fp)
+	{
+		if (info == NULL || fp == NULL) return false;
+
+		if (!ReadUint32(&info->infosize, fp)) return false;
+		if (!ReadInt32(&info->width, fp)) return false;
+		if (!ReadInt32(&info->height, fp)) return false;
+		if (!ReadUint16(&info->planes, fp)) return false;
+		if (!ReadUint16(&info->bits, fp)) return false;
+		if (!ReadUint32(&info->compression, fp)) return false;
+
+		return true;
+	}
+
+	bool ReadPalette(BMP_PALETTE* palette, FILE* fp)
+	{
+		if (palette == NULL || fp == NULL) return false;
+
+		if (!ReadUint8(&palette->b, fp)) return false;
+		if (!ReadUint8(&palette->g, fp)) return false;
+		if (!ReadUint8(&palette->r, fp)) return false;
+		if (!ReadUint8(&palette->a, fp)) return false;
+
+		return true;
+	}
+
+	bool Decode24(uint8_t* data, size_t size)
+	{
+		for (int i = 0; i < size; i += 3)
+		{
+			data[i + 0] = data[i + 2];
+			data[i + 1] = data[i + 0];
+			data[i + 2] = data[i - 2];
+		}
+
+		return true;
+	}
+
+	bool Decode(uint8_t* data, size_t size, size_t bits)
+	{
+		switch (bits)
+		{
+		case 24:
+			return Decode24(data, size);
+			break;
+		};
+
+		return true;
+	}
 
 	bool ClmbsImg_IsBMP(const char* file)
 	{
@@ -27,31 +143,25 @@ extern "C"
 	{
 		ClmbsImg_Data ret;
 
-	    FILE* fp = fopen(file, "rb");
-	    if (fp == NULL) return ret;
+		FILE* fp = fopen(file, "rb");
+		if (fp == NULL) return ret;
 
-	    unsigned char info[54];
-	    fread(info, sizeof(info), 1, fp);
+		BMP_HEADER header;
+		BMP_INFO info;
 
-	    int width = *(int*)&info[18];
-	    int height = *(int*)&info[22];
+		if (!ReadHeader(&header, fp)) return ret;
+		if (!ReadInfo(&info, fp)) return ret;
 
-	    int size = 3 * width * height;
-	    unsigned char* data = malloc(sizeof(unsigned char) * size);
-	    fread(data, sizeof(unsigned char), size, fp);
-	    fclose(fp);
+		ret.w = info.width;
+		ret.h = info.height;
+		ret.bpp = info.bits / 8;
 
-	   	for(size_t i = 0; i < size; i += 3)
-	    {
-            unsigned char tmp = data[i];
-            data[i] = data[i + 1];
-            data[i + 1] = tmp;
-	    }
+		uint8_t* data = malloc(header.size - 122);
+		fread(data, 1, header.size - 122, fp);
 
-	    ret.w = width;
-	    ret.h = height;
-	    ret.bpp = 3;
-	    ret.data = data;
+		Decode(data, header.size - 122, info.bits);
+
+		fclose(fp);
 
 		return ret;
 	}
